8333e98af8cc42210d59e6436769e87af0c2723d
Alina Mangusheva <alinamr@mail.ru>
2023-02-18 12:46:10 +0300

    feat: function to check string length added

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+//Функция для проверки длины строки
+function checkLengthString(str, len){
+  return (String(str).length <= len) ? 'true - строка проходит по длине' : 'false — строка не проходит';
+}
+
