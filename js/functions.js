d022a1e35f9109ad7314b0666ac95f70dd795497
Alina Mangusheva <alinamr@mail.ru>
2023-02-18 16:52:32 +0300

    feat: palindrome check function added

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,20 @@ function checkLengthString(str, len){
   return (String(str).length <= len) ? 'true - строка проходит по длине' : 'false — строка не проходит';
 }
 
+function isPalindrome(str){
+  for(let i = 0; i < str.length / 2; i++){
+    if(str[i].toLowerCase() !== str[str.length - 1 - i].toLowerCase()){
+      return 'false - это не палиндром';
+    }
+  }
+  for(let i = 0; i < str.length / 2; i++){
+    if(str[i] !== str[str.length - 1 - i]){
+      return 'true - несмотря на разный регистр, тоже палиндром';
+    }
+  }
+  return 'true - строка является палиндромом';
+
+
+}
+
+
